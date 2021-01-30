3b67889bc99376655043112150ed924fdccebd43
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-01-30 20:42:34 +0330

    fixed some bugs

--- app/src/main/java/com/majazeh/risloo/Views/Activities/SplashActivity.java
+++ app/src/main/java/com/majazeh/risloo/Views/Activities/SplashActivity.java
@@ -64,7 +64,7 @@ public class SplashActivity extends AppCompatActivity {
         WindowDecorator windowDecorator = new WindowDecorator();
 
         windowDecorator.darkShowSystemUI(this);
-        windowDecorator.darkSetSystemUIColor(this, getResources().getColor(R.color.Primary), getResources().getColor(R.color.Primary));
+        windowDecorator.darkSetSystemUIColor(this, getResources().getColor(R.color.CoralRed), getResources().getColor(R.color.CoralRed));
     }
 
     private void initializer() {
