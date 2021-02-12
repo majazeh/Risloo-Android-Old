361d476e2acffaf9bb673ccce734105c097f6f28
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-02-12 16:27:56 +0330

    cleaned account

--- app/src/main/java/com/majazeh/risloo/Views/Activities/AccountActivity.java
+++ app/src/main/java/com/majazeh/risloo/Views/Activities/AccountActivity.java
@@ -3,6 +3,7 @@ package com.majazeh.risloo.Views.Activities;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.widget.ImageViewCompat;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,9 +17,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -27,6 +26,7 @@ import android.widget.Toast;
 import com.majazeh.risloo.Utils.Generators.ExceptionGenerator;
 import com.majazeh.risloo.Models.Repositories.AuthRepository;
 import com.majazeh.risloo.R;
+import com.majazeh.risloo.Utils.Managers.ParamsManager;
 import com.majazeh.risloo.Utils.Widgets.ItemDecorateRecyclerView;
 import com.majazeh.risloo.Utils.Managers.WindowDecorator;
 import com.majazeh.risloo.ViewModels.AuthViewModel;
@@ -34,8 +34,6 @@ import com.majazeh.risloo.Views.Adapters.AccountAdapter;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
-
-import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -52,10 +50,10 @@ public class AccountActivity extends AppCompatActivity {
 
     // Widgets
     private RelativeLayout toolbarLayout;
-    private ImageView toolbarImageView, toolbarLogOutImageView, toolbarEditAccountImageView;
+    private ImageView toolbarImageView;
     private TextView toolbarTextView;
     private CircleImageView avatarCircleImageView;
-    private TextView nameTextView, cryptoTextView;
+    private TextView nameTextView, editTextView, logOutTextView;
     private RecyclerView accountRecyclerView;
     private Dialog logOutDialog, progressDialog;
     private TextView logOutDialogTitle, logOutDialogDescription, logOutDialogPositive, logOutDialogNegative;
@@ -91,20 +89,12 @@ public class AccountActivity extends AppCompatActivity {
 
         handler = new Handler();
 
-        toolbarLayout = findViewById(R.id.layout_toolbar_linearLayout);
+        toolbarLayout = findViewById(R.id.account_toolbar);
         toolbarLayout.setBackgroundColor(getResources().getColor(R.color.Snow));
 
         toolbarImageView = findViewById(R.id.layout_toolbar_primary_imageView);
         toolbarImageView.setImageResource(R.drawable.ic_chevron_right);
         ImageViewCompat.setImageTintList(toolbarImageView, AppCompatResources.getColorStateList(this, R.color.Nero));
-        toolbarLogOutImageView = findViewById(R.id.layout_toolbar_secondary_imageView);
-        toolbarLogOutImageView.setVisibility(View.VISIBLE);
-        toolbarLogOutImageView.setImageResource(R.drawable.ic_power_light);
-        ImageViewCompat.setImageTintList(toolbarLogOutImageView, AppCompatResources.getColorStateList(this, R.color.CoralRed));
-        toolbarEditAccountImageView = findViewById(R.id.layout_toolbar_thirdly_imageView);
-        toolbarEditAccountImageView.setVisibility(View.VISIBLE);
-        toolbarEditAccountImageView.setImageResource(R.drawable.ic_edit_light);
-        ImageViewCompat.setImageTintList(toolbarEditAccountImageView, AppCompatResources.getColorStateList(this, R.color.Nero));
 
         toolbarTextView = findViewById(R.id.layout_toolbar_textView);
         toolbarTextView.setText(getResources().getString(R.string.AccountTitle));
@@ -112,29 +102,26 @@ public class AccountActivity extends AppCompatActivity {
         avatarCircleImageView = findViewById(R.id.activity_account_avatar_circleImageView);
 
         nameTextView = findViewById(R.id.activity_account_name_textView);
-        cryptoTextView = findViewById(R.id.activity_account_crypto_textView);
+        editTextView = findViewById(R.id.activity_account_edit_textView);
+        logOutTextView = findViewById(R.id.activity_account_logout_textView);
 
-        accountRecyclerView = findViewById(R.id.activity_account_recyclerView);
+        accountRecyclerView = findViewById(R.id.account_recyclerView);
         accountRecyclerView.addItemDecoration(new ItemDecorateRecyclerView("verticalLayout", (int) getResources().getDimension(R.dimen._16sdp), (int) getResources().getDimension(R.dimen._8sdp), (int) getResources().getDimension(R.dimen._32sdp)));
         accountRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         accountRecyclerView.setHasFixedSize(true);
 
         logOutDialog = new Dialog(this, R.style.DialogTheme);
-        Objects.requireNonNull(logOutDialog.getWindow()).requestFeature(Window.FEATURE_NO_TITLE);
+        logOutDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         logOutDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         logOutDialog.setContentView(R.layout.dialog_action);
         logOutDialog.setCancelable(true);
+        logOutDialog.getWindow().setAttributes(ParamsManager.set(logOutDialog));
+
         progressDialog = new Dialog(this, R.style.DialogTheme);
-        Objects.requireNonNull(progressDialog.getWindow()).requestFeature(Window.FEATURE_NO_TITLE);
+        progressDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         progressDialog.setContentView(R.layout.dialog_progress);
         progressDialog.setCancelable(false);
-
-        WindowManager.LayoutParams layoutParamsLogOutDialog = new WindowManager.LayoutParams();
-        layoutParamsLogOutDialog.copyFrom(logOutDialog.getWindow().getAttributes());
-        layoutParamsLogOutDialog.width = WindowManager.LayoutParams.MATCH_PARENT;
-        layoutParamsLogOutDialog.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        logOutDialog.getWindow().setAttributes(layoutParamsLogOutDialog);
 
         logOutDialogTitle = logOutDialog.findViewById(R.id.dialog_action_title_textView);
         logOutDialogTitle.setText(getResources().getString(R.string.AccountLogOutDialogTitle));
@@ -150,10 +137,9 @@ public class AccountActivity extends AppCompatActivity {
     private void detector() {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             toolbarImageView.setBackgroundResource(R.drawable.draw_oval_solid_snow_ripple_quartz);
-            toolbarLogOutImageView.setBackgroundResource(R.drawable.draw_oval_solid_snow_ripple_quartz);
-            toolbarEditAccountImageView.setBackgroundResource(R.drawable.draw_oval_solid_snow_ripple_quartz);
 
-            cryptoTextView.setBackgroundResource(R.drawable.draw_8sdp_solid_snow_border_quartz_ripple_quartz);
+            editTextView.setBackgroundResource(R.drawable.draw_8sdp_solid_snow_border_quartz_ripple_quartz);
+            logOutTextView.setBackgroundResource(R.drawable.draw_8sdp_solid_snow_border_quartz_ripple_quartz);
 
             logOutDialogPositive.setBackgroundResource(R.drawable.draw_12sdp_solid_snow_ripple_quartz);
             logOutDialogNegative.setBackgroundResource(R.drawable.draw_12sdp_solid_snow_ripple_quartz);
@@ -169,42 +155,34 @@ public class AccountActivity extends AppCompatActivity {
             overridePendingTransition(R.anim.stay_still, R.anim.slide_out_bottom);
         });
 
-        toolbarLogOutImageView.setOnClickListener(v -> {
-            toolbarLogOutImageView.setClickable(false);
-            handler.postDelayed(() -> toolbarLogOutImageView.setClickable(true), 250);
-
-            logOutDialog.show();
-        });
-
-        toolbarEditAccountImageView.setOnClickListener(v -> {
-            toolbarEditAccountImageView.setClickable(false);
-            handler.postDelayed(() -> toolbarEditAccountImageView.setClickable(true), 250);
-
-            startActivityForResult(new Intent(this, EditAccountActivity.class), 100);
-            overridePendingTransition(R.anim.slide_in_bottom, R.anim.stay_still);
-        });
-
         avatarCircleImageView.setOnClickListener(v -> {
             avatarCircleImageView.setClickable(false);
             handler.postDelayed(() -> avatarCircleImageView.setClickable(true), 250);
 
             if (!authViewModel.getName().equals("") && !authViewModel.getAvatar().equals("")) {
-                Intent intent = (new Intent(this, ImageActivity.class));
+                Intent imageIntent = (new Intent(this, ImageActivity.class));
 
-                intent.putExtra("title", authViewModel.getName());
-                intent.putExtra("bitmap", false);
-                intent.putExtra("image", authViewModel.getAvatar());
+                imageIntent.putExtra("title", authViewModel.getName());
+                imageIntent.putExtra("bitmap", false);
+                imageIntent.putExtra("image", authViewModel.getAvatar());
 
-                startActivity(intent);
+                startActivity(imageIntent);
             }
         });
 
-        cryptoTextView.setOnClickListener(v -> {
-            cryptoTextView.setClickable(false);
-            handler.postDelayed(() -> cryptoTextView.setClickable(true), 250);
+        editTextView.setOnClickListener(v -> {
+            editTextView.setClickable(false);
+            handler.postDelayed(() -> editTextView.setClickable(true), 250);
 
-            startActivity(new Intent(this, CryptoActivity.class));
+            startActivityForResult(new Intent(this, EditAccountActivity.class), 100);
             overridePendingTransition(R.anim.slide_in_bottom, R.anim.stay_still);
+        });
+
+        logOutTextView.setOnClickListener(v -> {
+            logOutTextView.setClickable(false);
+            handler.postDelayed(() -> logOutTextView.setClickable(true), 250);
+
+            logOutDialog.show();
         });
 
         logOutDialogPositive.setOnClickListener(v -> {
@@ -217,7 +195,7 @@ public class AccountActivity extends AppCompatActivity {
 
         logOutDialogNegative.setOnClickListener(v -> {
             logOutDialogNegative.setClickable(false);
-            handler.postDelayed(() -> logOutDialogNegative.setClickable(true), 300);
+            handler.postDelayed(() -> logOutDialogNegative.setClickable(true), 250);
             logOutDialog.dismiss();
         });
 
@@ -230,7 +208,7 @@ public class AccountActivity extends AppCompatActivity {
             accountRecyclerView.setAdapter(accountAdapter);
 
             if (authViewModel.getAvatar().equals("")) {
-                avatarCircleImageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_user_circle_solid));
+                avatarCircleImageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_user_circle_solid, null));
             } else {
                 Picasso.get().load(authViewModel.getAvatar()).placeholder(R.color.Solitude).into(avatarCircleImageView);
             }
@@ -260,18 +238,24 @@ public class AccountActivity extends AppCompatActivity {
         AuthRepository.workState.observe((LifecycleOwner) this, integer -> {
             if (AuthRepository.work.equals("logOut")) {
                 if (integer == 1) {
-                    setResult(RESULT_OK, new Intent().putExtra("RESULT_STRING", AuthRepository.work));
+                    Intent result = new Intent();
+                    result.putExtra("RESULT_STRING", AuthRepository.work);
+
+                    setResult(RESULT_OK, result);
                     finish();
 
                     progressDialog.dismiss();
+
                     Toast.makeText(this, ExceptionGenerator.fa_message_text, Toast.LENGTH_SHORT).show();
                     AuthRepository.workState.removeObservers((LifecycleOwner) this);
                 } else if (integer == 0) {
                     progressDialog.dismiss();
+
                     Toast.makeText(this, ExceptionGenerator.fa_message_text, Toast.LENGTH_SHORT).show();
                     AuthRepository.workState.removeObservers((LifecycleOwner) this);
                 } else if (integer == -2) {
                     progressDialog.dismiss();
+
                     Toast.makeText(this, ExceptionGenerator.fa_message_text, Toast.LENGTH_SHORT).show();
                     AuthRepository.workState.removeObservers((LifecycleOwner) this);
                 }
@@ -285,7 +269,10 @@ public class AccountActivity extends AppCompatActivity {
 
         if (resultCode == RESULT_OK) {
             if (requestCode == 100) {
-                setResult(RESULT_OK, new Intent().putExtra("RESULT_STRING", AuthRepository.work));
+                Intent result = new Intent();
+                result.putExtra("RESULT_STRING", AuthRepository.work);
+
+                setResult(RESULT_OK, result);
                 setData();
             }
         }
